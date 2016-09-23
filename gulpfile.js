df876b88049905b49eb9d70f74be63ebed1c2cd8
Eric Debelak <ericdebelak@gmail.com>
2016-09-23 16:57:33 -0600

    finishing up all the gulp and browserSync stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require("gulp-ruby-sass");
+var sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('serve', ['sass'], function() {
@@ -13,16 +14,17 @@ gulp.task('serve', ['sass'], function() {
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return sass('style.sass', {sourcemap: true})
-     .on('error', function (err) {
-         console.error('Error!', err.message);
-     })
-     .pipe(sourcemaps.write('./', {
-         includeContent: false,
-         sourceRoot: './'
-     }))
-     .pipe(browserSync.stream({match: '*.css'}));
-});
+gulp.task('sass', () =>
+    sass('style.sass', {sourcemap: true})
+        .on('error', sass.logError)
+        // for inline sourcemaps
+        .pipe(sourcemaps.write())
+        // for file sourcemaps
+        .pipe(sourcemaps.write('', {
+            includeContent: false,
+            sourceRoot: 'source'
+        }))
+        .pipe(gulp.dest(''))
+);
 
 gulp.task('default', ['serve']);
