e2d1ec935612168452ff6a8cd23be1b7349ada2e
Eric Debelak <ericdebelak@gmail.com>
2017-07-01 10:36:45 +0300

    adding smooth scrolling by default

--- front-page.php
+++ front-page.php
@@ -18,23 +18,23 @@ add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_c
 add_action('genesis_after_header', 'add_home_page_widgets');
 function add_home_page_widgets() {
   genesis_widget_area( 'home-widget-1', array(
-		'before' => '<div class="home-widget-1 widget-area"><div class="wrap">',
+		'before' => '<div id="home-widget-1" class="home-widget-1 widget-area"><div class="wrap">',
 		'after'  => '</div></div>',
   ) );
   genesis_widget_area( 'home-widget-2', array(
-		'before' => '<div class="home-widget-2 widget-area"><div class="wrap">',
+		'before' => '<div id="home-widget-2" class="home-widget-2 widget-area"><div class="wrap">',
 		'after'  => '</div></div>',
   ) );
   genesis_widget_area( 'home-widget-3', array(
-		'before' => '<div class="home-widget-3 widget-area"><div class="wrap">',
+		'before' => '<div id="home-widget-3" class="home-widget-3 widget-area"><div class="wrap">',
 		'after'  => '</div></div>',
   ) );
   genesis_widget_area( 'home-widget-4', array(
-		'before' => '<div class="home-widget-4 widget-area"><div class="wrap">',
+		'before' => '<div id="home-widget-4" class="home-widget-4 widget-area"><div class="wrap">',
 		'after'  => '</div></div>',
   ) );
   genesis_widget_area( 'home-widget-5', array(
-		'before' => '<div class="home-widget-5 widget-area"><div class="wrap">',
+		'before' => '<div id="home-widget-5" class="home-widget-5 widget-area"><div class="wrap">',
 		'after'  => '</div></div>',
   ) );
 }
