3cba001336c703c63e5f0c5887edab063da2ef8d
Joshua T Garcia <josh@11online.us>
2017-09-07 18:05:05 -0600

    ADDED: prod and staging envs for gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -4,55 +4,78 @@ var sass        = require("gulp-ruby-sass");
 var sourcemaps  = require('gulp-sourcemaps');
 var rsync       = require('gulp-rsync');
 var config      = require('./config.json');
+var argv        = require('yargs').argv;
 
 
 gulp.task('serve', ['sass'], function() {
     browserSync.init(null, {
         proxy: config.proxy,
         ghostMode: {
-          clicks: true,
-          location: true,
-          forms: true,
-          scroll: true,
+            clicks: true,
+            location: true,
+            forms: true,
+            scroll: true,
         }
     });
 
     gulp.watch("style.sass", ['sass']);
     gulp.watch("sass-partials/*", ['sass']);
+    gulp.watch("sass-partials/**/*", ['sass']);
     gulp.watch("./*").on('change', browserSync.reload);
 });
 
 gulp.task('watch', ['sass'], function() {
+
     gulp.watch("style.sass", ['sass']);
     gulp.watch("sass-partials/*", ['sass']);
+    gulp.watch("sass-partials/**/*", ['sass']);
+
 });
+
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', () =>
-    sass('style.sass', {sourcemap: true})
-        .on('error', sass.logError)
-        // for file sourcemaps
-        .pipe(sourcemaps.write('maps'), {
-          includeContent: false,
-          sourceRoot: 'source'
-        })
-        .pipe(gulp.dest(''))
+sass('style.sass', {sourcemap: true})
+    .on('error', sass.logError)
+    // for file sourcemaps
+    .pipe(sourcemaps.write('maps'), {
+        includeContent: false,
+        sourceRoot: 'source'
+    })
+    .pipe(gulp.dest(''))
 );
 
+/*
+ * deploy the code to server
+ *
+ * `gulp deploy --prod` will deploy to production
+ * `gulp deploy --staging` will deploy to staging
+ */
 gulp.task('deploy', ['sass'], function() {
-  return gulp.src('*')
-    .pipe(rsync({
-      root: '',
-      hostname: config.hostname,
-      username: 'serverpilot',
-      destination: config.destination,
-      silent: false,
-      compress: true,
-      incremental: true,
-      emptyDirectories: true,
-      recursive: true,
-      exclude: ['node_modules', '.sass-cache', 'maps', 'sass-partials', 'config.json', 'gulpfile.js', 'package.json', 'style.sass']
-    }));
+
+    var hostname = config.staging.hostname,
+    username = config.staging.username,
+    destination = config.staging.destination;
+
+    if(argv.production || argv.prod) {
+        hostname = config.production.hostname,
+        username = config.production.username,
+        destination = config.production.destination;
+    }
+
+    return gulp.src('*')
+        .pipe(rsync({
+            root: '',
+            hostname: hostname,
+            username: username,
+            destination: destination,
+            silent: false,
+            compress: true,
+            incremental: true,
+            emptyDirectories: true,
+            recursive: true,
+            exclude: ['node_modules', '.sass-cache', 'maps', 'sass-partials', 'config.json', 'gulpfile.js', 'package.json', 'style.sass']
+        }));
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['sass']);
